1ed6d23419aad6f3885d8415e54d1e9ced7ecbe0
Jaz Lim <angmeng@gmail.com>
2015-06-10 02:36:42 +0800

    Add slug to Admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608104147) do
+ActiveRecord::Schema.define(version: 20150609183549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,12 @@ ActiveRecord::Schema.define(version: 20150608104147) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "slug"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  add_index "admins", ["slug"], name: "index_admins_on_slug", unique: true, using: :btree
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
