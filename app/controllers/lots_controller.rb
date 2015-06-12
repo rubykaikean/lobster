9730fd047ea019f733ecbf79bc1ef625fbf287ad
rubykaikean <khongkk89@gmail.com>
2015-06-12 10:54:34 +0800

    commit to master

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -73,7 +73,7 @@ class LotsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_lot
-      @lot = Lot.find(params[:id])
+      @lot = Lot.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
