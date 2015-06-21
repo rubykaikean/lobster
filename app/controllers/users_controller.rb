5269c5dc5ffc30b46e28d81d0f5fbb8d1b0a7020
Jaz Lim <angmeng@gmail.com>
2015-06-21 22:37:17 +0800

    Add font awesome icon in menu items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :authenticate_project_owner!
   before_action :set_user, only: [:show, :edit, :update_member, :destroy]
 
   # GET /users
