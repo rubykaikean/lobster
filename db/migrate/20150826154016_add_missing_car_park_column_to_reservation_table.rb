663299e1737943a1f711cbe8d40a53d146d3ddc9
rubykaikean <khongkk89@gmail.com>
2015-08-26 23:45:49 +0800

    master add missing car park column migration

---
+++ db/migrate/20150826154016_add_missing_car_park_column_to_reservation_table.rb
@@ -0,0 +1,6 @@
+class AddMissingCarParkColumnToReservationTable < ActiveRecord::Migration
+  def change
+  	add_column :reservation_customizations, :car_park, :boolean, default: false
+  	add_column :reservation_customizations, :car_park_required, :boolean, default: false
+  end
+end
