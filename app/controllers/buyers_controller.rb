f773e11d3f6d7c78e800191c39b90e8167b6b392
rubykaikean <khongkk89@gmail.com>
2015-09-07 13:51:21 +0800

    add required checing on sale buyer edit form

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -30,6 +30,7 @@ class BuyersController < ApplicationController
   # GET /buyers/1/edit
   def edit
     @sale = Sale.find(params[:sale_id])
+    @customization = @sale.product.reservation_customization
     if is_top_level_management? || @sale.user_id == current_user.id
       @region = @sale.product.regions
       @sourcestype = @sale.product.sources_types
