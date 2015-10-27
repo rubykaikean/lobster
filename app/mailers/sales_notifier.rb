97465c88ebf619bf4b7a6398926a1ee066414127
rubykaikean <khongkk89@gmail.com>
2015-10-27 15:49:05 +0800

    set setting unit booked hide price

--- app/mailers/sales_notifier.rb
+++ app/mailers/sales_notifier.rb
@@ -20,4 +20,9 @@ class SalesNotifier < ApplicationMailer
     admin_emails = @sale.project.company.users.select {|user| user.is_admin? }.map {|user| user.email }.join(", ")
     mail(to: "#{admin_emails}", subject: "New confirmed sale!", from: "#{email_template.from}")
   end
+
+  # def inform_agent(sale_id)
+  #   @sale = Sale.find_by(id: sale_id)
+  #   @lot = @sale.lot
+  # end
 end
