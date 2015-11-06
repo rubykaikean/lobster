ad624acb3a5f9718b6d2c6b48e33d91f10fc4a0d
rubykaikean <khongkk89@gmail.com>
2015-11-06 15:12:33 +0800

    add payment detail column into payment table

--- app/mailers/sales_notifier.rb
+++ app/mailers/sales_notifier.rb
@@ -17,7 +17,7 @@ class SalesNotifier < ApplicationMailer
     @buyer = @sale.buyer
     @product = @sale.product
     email_template = @product.email_setting
-    admin_emails = @sale.project.company.users.select {|user| user.is_admin? }.map {|user| user.email }.join(", ")
+    admin_emails = @sale.project.company.users.select {|user| user.is_admin? if (user.status_id == 1) }.map {|user| user.email }.join(", ")
     mail(to: "#{admin_emails}", subject: "New confirmed sale!", from: "#{email_template.from}")
   end
 
@@ -25,7 +25,7 @@ class SalesNotifier < ApplicationMailer
     @sale = Sale.find_by(id: sale_id)
     lot = @sale.lot
     email_template = @sale.product.email_setting
-    agent_emails = @sale.project.company.users.map {|user| user.email}.join(", ")
+    agent_emails = @sale.project.company.users.map {|user| user.email if (user.status_id == 1)}.join(", ")
     mail(to: "#{agent_emails}", subject: "#{email_template.subject}", from: "#{email_template.from}", body: "#{lot.name} unit had been booked!")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105061219) do
+ActiveRecord::Schema.define(version: 20151106061716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -223,6 +223,8 @@ ActiveRecord::Schema.define(version: 20151105061219) do
     t.datetime "image_updated_at"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "cheque_number"
+    t.string   "transaction_number"
   end
 
   create_table "phases", force: :cascade do |t|
