59e8c1b43039bb1e5be7418325eeff78029847c6
rubykaikean <khongkk89@gmail.com>
2015-11-09 15:37:40 +0800

    rename prefixed varible

--- app/services/email_template_renderer.rb
+++ app/services/email_template_renderer.rb
@@ -71,7 +71,7 @@ class EmailTemplateRenderer
     PREFIXES[:PRODUCT].each do |attr|
       self.body.gsub!("[product_#{attr}]", self.product.public_send("#{attr}").to_s)
     end
-    PREFIXES[:USER].each do |attr|
+    PREFIXES[:AGENT].each do |attr|
       self.body.gsub!("[agent_#{attr}", self.user.public_send("#{attr}").to_s)
     end
     PREFIXES[:COMPANY].each do |attr|
