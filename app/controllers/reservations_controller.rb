68e5c795cb9f89983a12633d93fc47dac2e40f2d
rubykaikean <khongkk89@gmail.com>
2015-11-20 15:58:10 +0800

    temporary remark customization sale person

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -21,25 +21,25 @@ class ReservationsController < ApplicationController
   end
 
   def buyer
-    render :text => params
-    # @lot = Lot.friendly.find(params[:id])
-    # if UserAccessible.new(current_user, :reservation, :reserve).can_access?
-    #   @product = @lot.product
-    #   @sourcestype = @product.sources_types
-    #   @region = @product.regions
-    #   @setting = @product.product_setting
-    #   @customization = @product.reservation_customization
-    #   if @lot.available_for_booking?
-    #     @buyer = Buyer.new
-    #     @sale = Sale.new
-    #   else
-    #     flash[:alert] = "Lot #{@lot.name} is already reserved."
-    #     redirect_to reservation_path(@lot.product)
-    #   end
-    # else
-    #   flash[:alert] = "Sorry, you don't have the access right."
-    #   redirect_to reservation_path(@lot.product)
-    # end
+    # render :text => params
+    @lot = Lot.friendly.find(params[:id])
+    if UserAccessible.new(current_user, :reservation, :reserve).can_access?
+      @product = @lot.product
+      @sourcestype = @product.sources_types
+      @region = @product.regions
+      @setting = @product.product_setting
+      @customization = @product.reservation_customization
+      if @lot.available_for_booking?
+        @buyer = Buyer.new
+        @sale = Sale.new
+      else
+        flash[:alert] = "Lot #{@lot.name} is already reserved."
+        redirect_to reservation_path(@lot.product)
+      end
+    else
+      flash[:alert] = "Sorry, you don't have the access right."
+      redirect_to reservation_path(@lot.product)
+    end
   end
 
   # confirm booking and hide book unit button
@@ -49,41 +49,42 @@ class ReservationsController < ApplicationController
   end
 
   def create_lot
-    @lot = Lot.find(params[:lot_id])
-    @sourcestype = @lot.product.sources_types
-    @region = @lot.product.regions
-    @setting = @lot.product.product_setting
-    if UserAccessible.new(current_user, :reservation, :reserve).can_access?
-      data = { 
-        lot: @lot, 
-        setting: @setting,
-        buyer_data: buyer_params,
-        payment_type_id: params[:payment_type_id],
-        booking_fee: params[:booking_fee],
-        cheque_number: params[:cheque_number],
-        transaction_number: params[:transaction_number],
-        payment_image: params[:payment_image],
-        user_id: params[:user_id]
-      }
-      # result = SaleEngine.reserve(data)
-      result = CustomSaleEngine.reserve(data)
-      case result[:status]
-      when 201
-        flash[:notice] = result[:message]
-        redirect_to reservation_path(@lot.product)
-      when 403
-        flash[:alert] = result[:message]
-        redirect_to reservation_path(@lot.product)
-      when 400
-        flash.now[:alert] = result[:message]
-        @buyer = result[:buyer]
-        @sale = result[:sale]
-        render action: 'buyer'
-      end
-    else
-      flash[:alert] = "Sorry, you don't have the access right."
-      redirect_to reservation_path(@lot.product)
-    end
+    render :text => params.to_json
+    # @lot = Lot.find(params[:lot_id])
+    # @sourcestype = @lot.product.sources_types
+    # @region = @lot.product.regions
+    # @setting = @lot.product.product_setting
+    # if UserAccessible.new(current_user, :reservation, :reserve).can_access?
+    #   data = { 
+    #     lot: @lot, 
+    #     setting: @setting,
+    #     buyer_data: buyer_params,
+    #     payment_type_id: params[:payment_type_id],
+    #     booking_fee: params[:booking_fee],
+    #     cheque_number: params[:cheque_number],
+    #     transaction_number: params[:transaction_number],
+    #     payment_image: params[:payment_image],
+    #     user_id: params[:user_id]
+    #   }
+    #   # result = SaleEngine.reserve(data)
+    #   result = CustomSaleEngine.reserve(data)
+    #   case result[:status]
+    #   when 201
+    #     flash[:notice] = result[:message]
+    #     redirect_to reservation_path(@lot.product)
+    #   when 403
+    #     flash[:alert] = result[:message]
+    #     redirect_to reservation_path(@lot.product)
+    #   when 400
+    #     flash.now[:alert] = result[:message]
+    #     @buyer = result[:buyer]
+    #     @sale = result[:sale]
+    #     render action: 'buyer'
+    #   end
+    # else
+    #   flash[:alert] = "Sorry, you don't have the access right."
+    #   redirect_to reservation_path(@lot.product)
+    # end
   end
 
 
