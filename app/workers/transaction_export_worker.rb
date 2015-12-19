9c05697b57d68d7cb018360f25c9935f120a0f1b
rubykaikean <khongkk89@gmail.com>
2015-12-19 14:56:36 +0800

    test send to eversolf test API

--- app/workers/transaction_export_worker.rb
+++ app/workers/transaction_export_worker.rb
@@ -23,7 +23,7 @@ class TransactionExportWorker
           transaction_number: data[:transaction_number]
         }
     # result = RestClient.post "http://117.53.153.87:8889/postprebook", {"booking": {"transaction_id": "123","full_name": "admin"}}.to_json, :content_type => :json, :accept => :json
-    result = RestClient.post "http://117.53.153.87:8889/postprebook", booking.to_json, :content_type => :json, :accept => :json
+    result = RestClient.post "http://117.53.153.87:8800", booking.to_json, :content_type => :json, :accept => :json
     doc = JSON.parse result
     # doc.class
     if doc["PostPrebook_response"]["Result"]["BookingSuccess"].to_i == 0
