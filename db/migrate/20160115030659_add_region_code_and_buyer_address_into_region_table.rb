e62fcb1301bd3c0e88ae5d2c2521749cca202ba9
rubykaikean <khongkk89@gmail.com>
2016-01-18 15:30:55 +0800

    add two buyer address

---
+++ db/migrate/20160115030659_add_region_code_and_buyer_address_into_region_table.rb
@@ -0,0 +1,21 @@
+class AddRegionCodeAndBuyerAddressIntoRegionTable < ActiveRecord::Migration
+  
+	def up
+		add_column :regions, :region_code, :string, :limit => 3
+
+  	add_column :buyers, :buyer_second_address, :string, :limit => 40
+
+  	add_column :buyers, :buyer_third_address, :string, :limit => 40		
+	
+  	change_column :buyers, :address, :string, :limit => 40
+	end
+
+	def down
+
+		remove_column :regions, :region_code
+		remove_column :buyers, :buyer_second_address
+		remove_column :buyers, :buyer_third_address
+		change_column :buyers, :address, :text
+	end
+
+end
