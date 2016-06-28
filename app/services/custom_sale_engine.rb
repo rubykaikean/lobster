1db502f86d0a902fb0a49abdb908b2c586f078a9
gingerseller <khongkk89@gmail.com>
2016-06-28 13:36:18 +0800

    return back on live

--- app/services/custom_sale_engine.rb
+++ app/services/custom_sale_engine.rb
@@ -57,7 +57,8 @@ class CustomSaleEngine
       
       group_data = {:booking => ""}
       group_data[:booking] = record
-      binding.pry
+      # binding.pry
+      # puts group_data
       # http://117.53.153.87:8889/postprebook >> testing
       # result_respond = RestClient.post "http://117.53.153.87:8800/postprebook", {"booking": [{"transaction_id": "1231", "full_name": "asdasd","buyer_second_name": "","buyer_third_name": "", "buyer_ic_number": "8989898989","second_buyer_ic_number": "", "third_buyer_ic_number": "","buyer_address": "No.27 Jalan Tiang Seri U8/73G Bukit Jelutong 41050 Shah Alam Selangor", "buyer_postcode": "4142342","booking_fee": "", "car_park_unit": "","payment_type": "", "lot_number": "","selling_price": "", "cheque_number": "", "transaction_number": ""}]}.to_json, :content_type => :json, :accept => :json
       
