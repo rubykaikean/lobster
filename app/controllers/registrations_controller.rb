494ae2cbfc635691df60d1a1263ba7ca009a4cdc
gingerseller <khongkk89@gmail.com>
2017-02-07 09:56:50 +0800

    commit and checkout to master

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  layout "application"
+  layout "devise", only: [:new]
+  # layout "application"
 
 	def new
-    render :text => params
+    # render :text => params
 		# @company = Company.new
-    # self.resource = User.new
+    self.resource = User.new
     # raise ActiveRecord::RecordNotFound
-    raise ActionController::RoutingError.new('Not Found')
+    # raise ActionController::RoutingError.new('Not Found')
   end
 
   def create
-  	# render :text => params
-    # user = User.new(sign_up_params)
+    user = User.new(sign_up_params)
+    user.status_id = User::INACTIVE
+    user.type_id   = User::STAFF
+    user.save!
+    redirect_to new_user_session_path, :notice => "Thank for signed up"
+    # continue status id and type id 
 		# @company = Company.new(company_params)
 		# @company.type_id = params[:type_id]
 
-    raise ActionController::RoutingError.new('Not Found')
+    # raise ActionController::RoutingError.new('Not Found')
   end
 
   def edit
@@ -30,6 +35,7 @@ class RegistrationsController < Devise::RegistrationsController
     edit_user_registration_path
   end
 
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) do |u|
       u.permit(:username, :email, :display_name, :contact_number, :type_id, :company_id, :status_id, :password, :password_confirmation, :current_password)
@@ -38,6 +44,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   # private
 
+  # def sign_up_params
+  #   params.require(:user).permit(:email, :display_name, :password, :password_confirmation)
+  # end
   # def company_params
   #     params.require(:company).permit(:name, :registration_number, :address, :phone_number, :fax_number, :type_id, :slug)
   # end
