a51756171380c74c3e02b6ea58714c13fd97b835
gingerseller <khongkk89@gmail.com>
2017-06-15 18:34:31 +0800

    change v2 folder to v3

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Lobster
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.enabled = false
+    
 
     config.active_job.queue_adapter = :sidekiq
 
