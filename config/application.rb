2dc3677d14f0b8d5a226999ef58ce5b7ad5418dd
gingerseller <khongkk89@gmail.com>
2017-06-15 18:31:10 +0800

    change v2 folder to v3

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Lobster
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.enabled = false
+
     config.active_job.queue_adapter = :sidekiq
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
